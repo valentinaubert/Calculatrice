ccdb80ba00abe116df974ede6a0a2bcf94d28257
Lele26 <idreamxlea@gmail.com>
2020-03-23 16:49:16 +0100

    Ajout verif touche du clavier

--- Calculatrice/MainWindow.xaml.cs
+++ Calculatrice/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -33,13 +34,16 @@ namespace Calculatrice
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Button b = (Button)sender;
+            System.Windows.Controls.Button b = (System.Windows.Controls.Button)sender;
             _vm.Resultat = b.Content.ToString();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-
+            if (e.Key == Key.Enter)
+            {
+                _vm.Resultat = "ENTER";
+            }
         }
     }
 }
